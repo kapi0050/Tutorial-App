95aa459e01c448e608769707f11c2f4284bb741d
koideteruhiko <kapi003@gmail.com>
2019-02-26 14:50:52 +0000

    Finish user edit index destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,20 +4,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # user = User.find_by(params[:email])#うまくいくがsessionを使えていない
-    
-    user = User.find_by(email: params[:session][:email].downcase)#テキストログインできない不具合
-    # User.find_by(email: "aaaa@mail.com")
-    #エラー内容-User.find_by...の中身を確認
-    #User Load (0.2ms)  SELECT  "users".* FROM "users" WHERE "users"."email" = ? LIMIT ?  [["email", "aaaa@gmail.com"], ["LIMIT", 1]]
-    #=> nil
-    
-    #user = User.find_by(params[:id])#testログインできることを確認
-    #user = User.find_by(params[:session][:email])#エラー
-    #binding.pry
+    user = User.find_by(email: params[:session][:email].downcase)
+
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:danger] = 'メールアドレスとパスワードの情報が一致しませんでした。'
       render 'new'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,25 +1,41 @@
 module SessionsHelper
-  
-  #引数に渡されたユーザーでログインする
+
+  # 引数に渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
   end
-  
-  #現在ログイン中のユーザーを返す
+
+  # 現在ログイン中のユーザーを返す (ただし、いる場合のみ)
   def current_user
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
     end
   end
-  
+
+  # 渡されたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
+
   # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
     !current_user.nil?
   end
-  
-  #現在のユーザーをログアウトする
+
+  # 現在のユーザーをログアウトする
   def log_out
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  #記録している・・・
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwading_url)
+  end
+  
+  # アクセスしよう・・
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
