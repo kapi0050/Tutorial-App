facd0a016d42a62d3db778961d1bb46ea56b8459
koideteruhiko <kapi003@gmail.com>
2019-03-06 08:13:42 +0000

    10章おわり一応

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -25,4 +25,20 @@ module AttendancesHelper
     @user.attendances.where('worked_on >= ? and worked_on <= ?', @first_day, @last_day).order('worked_on')
   end
   
+  def attendances_invalid?
+    attendances = true
+    attendances_params.each do |id, item|
+      if item[:started_at].blank? && item[:finished_at].blank?  
+        next
+      elsif item[:started_at].blank? || item[:finished_at].blank?
+        attendances = false
+        break
+      elsif item[:started_at].to_s > item[:finished_at].to_s
+        attendances = false
+        break
+      end
+    end
+    attendances
+  end
+  
 end
